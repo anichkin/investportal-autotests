4c3f221687e841537b92bb002bc88a16d75cf8aa
Crypt4Fan <nikolaj-gajnulin@yandex.ru>
2020-09-15 21:40:51 +0800

    fix login

--- src/autotests/pages/MainPage.py
+++ src/autotests/pages/MainPage.py
@@ -1,19 +1,28 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class MainPage:
 
     URL = 'https://investmoscow.upt24.ru/'
-    LOGIN_PIC = (By.XPATH, '//*[@id="auth-bt"]/a')
+    LOGIN_PIC = (By.ID, 'auth-btns')
+    LOGIN_INPUT = (By.ID, 'Login')
+    PASSWORD_INPUT = (By.ID, 'Password')
+    LOGIN = 'ilyaaanichkin@yandex.ru'
+    PASSWORD = '1234567'
 
     def __init__(self, driver):
 
         self.driver = driver
-        self.driver.get(MainPage.URL)
 
     def get_title(self):
         return self.driver.title
 
     def press_to_login_pic(self):
-        login_pic = self.driver.find_element(*MainPage.LOGIN_PIC)
+
+        login_pic = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(MainPage.LOGIN_PIC))
         login_pic.click()
+        login_input = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(MainPage.LOGIN_INPUT))
+        password_input = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(MainPage.PASSWORD_INPUT))
+        return login_input, password_input
--- src/autotests/tests/test_MainPage.py
+++ src/autotests/tests/test_MainPage.py
@@ -6,9 +6,12 @@ from autotests.pages.MainPage import MainPage
 def test_MainPage():
 
     driver = webdriver.Chrome()
+    driver.get(MainPage.URL)
     main_page = MainPage(driver)
     assert main_page.get_title() == 'Главная - Инвестиционный портал Москвы'
-    main_page.press_to_login_pic()
+    login_input, password_input = main_page.press_to_login_pic()
+    login_input.send_keys(MainPage.LOGIN)
+    password_input.send_keys(MainPage.PASSWORD)
 
 
 if __name__ == "__main__":
