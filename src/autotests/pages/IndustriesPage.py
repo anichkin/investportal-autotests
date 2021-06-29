3a79ebeebe118c523113000c5f4fed6bd54b064e
user <user@user.user>
2021-06-29 23:47:19 +0800

    Промышленность

--- src/autotests/pages/IndustriesPage.py
+++ src/autotests/pages/IndustriesPage.py
@@ -7,11 +7,14 @@ class IndustriesPage:
 
     URL = 'https://investmoscow.ru/industries'
     TITLE = 'Промышленности - Инвестиционный портал Москвы'
-    MOSCOW_TECHNICAL_SCHOOL = (By.XPATH)
-
-
-
-
+    MOSCOW_TECHNICAL_SCHOOL = (By.XPATH, '//*[@id="invest-moscow-app"]/div[9]/div')
+    BANNER_TECH_SCOOL = (By.XPATH, '//*[@id="invest-moscow-app"]/div[13]')
+    PRODUCTION = (By.XPATH, '//*[@id="invest-moscow-app"]/div[16]/div')
+    PROM_BANNER = (By.XPATH, '//*[@id="invest-moscow-app"]/div[20]')
+    BANK = (By.XPATH, '//*[@id="invest-moscow-app"]/div[23]/div')
+    INDUSTRIES_BANNER = (By.XPATH, '//*[@id="invest-moscow-app"]/div[27]')
+    EVOLUTION_CONTENT = (By.XPATH, '//*[@id="invest-moscow-app"]/div[28]')
+    ANALYTICAL_MATERIALS = (By.XPATH, '//*[@id="invest-moscow-app"]/div[30]')
 
     def __init__(self, driver):
         '''Вызывается при создании PageObject'''
@@ -22,3 +25,17 @@ class IndustriesPage:
         '''Функция получения тайтла страницы'''
 
         return self.driver.title
+
+        
+    def check_blocks(self):
+        '''Проверка отображения всех блоков на странице'''
+
+        WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(IndustriesPage.MOSCOW_TECHNICAL_SCHOOL))
+        WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(IndustriesPage.BANNER_TECH_SCOOL))
+        WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(IndustriesPage.PRODUCTION))
+        WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(IndustriesPage.PROM_BANNER))
+        WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(IndustriesPage.BANK))
+        WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(IndustriesPage.INDUSTRIES_BANNER))
+        WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(IndustriesPage.EVOLUTION_CONTENT))
+        WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(IndustriesPage.ANALYTICAL_MATERIALS))
+        
