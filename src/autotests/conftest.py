4953ed1af90b931a23b6ea6455bdb923a8f13e39
ilyaaanichkin <xpapapapokerfacex@gmail.com>
2022-05-19 21:22:50 +0300

    исправление фокуса на классификатор

--- src/autotests/conftest.py
+++ src/autotests/conftest.py
@@ -31,7 +31,7 @@ def driver():
     options = webdriver.ChromeOptions()
     user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36'
     options.add_argument(f'user-agent={user_agent}')
-    #options.add_argument('--headless')
+    options.add_argument('--headless')
     #    options.add_argument('--disable-blink-features=AutomationControlled')
     #    options.add_argument("--disable-extensions")
     #options.add_argument('--no-sandbox')
