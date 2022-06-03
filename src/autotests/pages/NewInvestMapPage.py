8bd6956db3fde42d8c52f170724a04122785c4c5
ilyaaanichkin <xpapapapokerfacex@gmail.com>
2022-06-03 18:45:47 +0300

    добавление теста новой инвесткарты и добавление теста НМП калькулятора

---
+++ src/autotests/pages/NewInvestMapPage.py
@@ -0,0 +1,9 @@
+from .base_page import BasePage
+
+
+class NewInvestMapPage(BasePage):
+    PATH = '/about-moscow/investment-map-v2/'
+    TITLE = 'Инвестиционная карта - Инвестиционный портал Москвы'
+    INVESTMAP_AREA = '//*[@id="invest-map"]'
+    SIDEBAR_AREA = '//*[@id="uid-portal"]/div/div[1]/div[2]/div[2]/div'
+    
