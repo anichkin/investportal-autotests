b472ed7531e757ec6da3833c85457e734d057c8e
ilyaaanichkin <xpapapapokerfacex@gmail.com>
2022-06-19 22:53:52 +0300

    добавление возможности множественного запуска теста

--- src/autotests/conftest.py
+++ src/autotests/conftest.py
@@ -32,6 +32,26 @@ def pytest_addoption(parser):
              'https://web3dgp.investmoscow.ru/, '
 
     )
+    parser.addoption('--repeat', action='store',
+                     help='Number of times to repeat each test')
+
+
+def pytest_generate_tests(metafunc):
+    """функция для отладки теста, чтобы запустить его несколько раз подряд
+    пример запуска: pytest.exe src\autotests\tests\test_moscow_tech_school.py --repeat 10
+    в примере тест запустится 10 раз подряд"""
+    if metafunc.config.option.repeat is not None:
+        count = int(metafunc.config.option.repeat)
+
+        # We're going to duplicate these tests by parametrizing them,
+        # which requires that each test has a fixture to accept the parameter.
+        # We can add a new fixture like so:
+        metafunc.fixturenames.append('tmp_ct')
+
+        # Now we parametrize. This is what happens when we do e.g.,
+        # @pytest.mark.parametrize('tmp_ct', range(count))
+        # def test_foo(): pass
+        metafunc.parametrize('tmp_ct', range(count))
 
 
 
